f459ebc94afbc11cada94c4a25fe28bfd2d3e8c8
Abhishek Pratap Singh <35230163+Prime-5@users.noreply.github.com>
2020-01-08 19:14:01 +0530

    Run `scrapy crawl goodread` in the terminal to execute

---
+++ 01. Goodreads/goodread.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class GoodreadSpider(scrapy.Spider):
+    name = 'goodread'
+    allowed_domains = ['www.goodreads.com/quotes/']
+    start_urls = ['https://www.goodreads.com/quotes/']
+
+    def parse(self, response):
+        quotes = response.xpath('//*[@class="quote"]')
+        for quote in quotes:
+        	text = quote.xpath('.//*[@class="quoteText"]/text()').extract_first()
+        	author = quote.xpath('.//*[@class="authorOrTitle"]/text()').extract_first()
+        	
+        	# print(text)
+        	# print(author)
+        	# print('\n')
+
+        	yield{	'Text': text,
+        			'Author': author}
+        next_page_url = response.xpath('//*[@class="next_page"]/@href').extract_first()
+        absolute_next_page_url = response.urljoin(next_page_url)
+        yield scrapy.Request(absolute_next_page_url, dont_filter=True)
+
