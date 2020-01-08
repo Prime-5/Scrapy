edaa72f55010078cbbec4fd33c0e4157d7c3bba9
Abhishek Pratap Singh <35230163+Prime-5@users.noreply.github.com>
2020-01-08 19:33:05 +0530

    Updating None condition for termination

--- 01. Goodreads/goodread.py
+++ 01. Goodreads/goodread.py
@@ -20,6 +20,6 @@ class GoodreadSpider(scrapy.Spider):
         	yield{	'Text': text,
         			'Author': author}
         next_page_url = response.xpath('//*[@class="next_page"]/@href').extract_first()
-        if(next_page_url!=''):
+        if next_page_url is not None:
         	absolute_next_page_url = response.urljoin(next_page_url)
         	yield scrapy.Request(absolute_next_page_url, dont_filter=True)
